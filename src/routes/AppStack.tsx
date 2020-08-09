d477dcc3a66b594c42e62ca966f13d5fe6b52674
gustavo2892 <gustavo2892@gmail.com>
2020-08-09 14:42:10 -0300

    GiveClasses Page

--- src/routes/AppStack.tsx
+++ src/routes/AppStack.tsx
@@ -4,6 +4,7 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
 import Landing from '../pages/Landing';
+import GiveClasses from '../pages/GiveClasses';
 
 const { Navigator, Screen } = createStackNavigator();
 
@@ -12,6 +13,7 @@ function AppStack() {
     <NavigationContainer>
       <Navigator screenOptions={{ headerShown: false }}>
         <Screen name="Landing" component={Landing} />
+        <Screen name="GiveClasses" component={GiveClasses} />
       </Navigator>
     </NavigationContainer>
   );
