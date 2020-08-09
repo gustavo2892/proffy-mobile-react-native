1386824edc8e4b4579358dad4f78e0fddff449fb
gustavo2892 <gustavo2892@gmail.com>
2020-08-09 15:24:29 -0300

    Config Bottom Tab Navigator

---
+++ src/pages/Favorites/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import styles from './styles';
+
+const Favorites: React.FC = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Favorites Page</Text>
+    </View>
+  );
+};
+
+export default Favorites;
---
+++ src/pages/TeacherList/index.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+import style from './styles';
+
+const TeacherList: React.FC = () => {
+  return (
+    <View style={style.container}>
+      <Text>TeacherList Page</Text>
+    </View>
+  );
+};
+
+export default TeacherList;
